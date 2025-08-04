d01a807d4c142082801497892b4214c84a9dfd24
karfa113 <karfamonojit3117@gmail.com>
2025-08-04 21:16:44 +0530

    Day 01

---
+++ Codes/D1P3.js
@@ -0,0 +1,19 @@
+/*let roll = "113"
+console.log(typeof roll);
+console.log(roll);
+
+roll = Number(roll)
+console.log(typeof roll)
+console.log(roll);*/
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false = 0
+
+let bakclog = " "
+bakclog = Boolean(bakclog)
+console.log(bakclog);
+
+// 0 => false; 1 => true
+// "" => false
+// "statement" => true
