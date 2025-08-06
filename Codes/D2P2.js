51f7c751ca6fe3ddd0d911d6f1222ca4eb61ac48
karfa113 <karfamonojit3117@gmail.com>
2025-08-06 20:32:32 +0530

    Day 02

---
+++ Codes/D2P2.js
@@ -0,0 +1,10 @@
+// Comparison in JS
+
+console.log("2" === 2); // use === to check value as well as data type
+
+
+// Premetive data types:
+// 7 types: string, number, boolean, null, symbol, bigint, undefined
+
+// Reference (Non-Premetive data types):
+// Arrays, objects and functions
