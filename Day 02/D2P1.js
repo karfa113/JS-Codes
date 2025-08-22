7244856c76ce87e1c518201ac0d71e90b8b05c98
karfa113 <karfamonojit3117@gmail.com>
2025-08-22 12:26:53 +0530

    Day 02

---
+++ Day 02/D2P1.js
@@ -0,0 +1,16 @@
+/* Basic math operations:
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**2)*/
+
+let name = "Monojit"
+let space = " "
+let surname = "Karfa"
+let fullName = name + space + surname
+console.log(fullName)
+
+// Prefix and postfix is available here
