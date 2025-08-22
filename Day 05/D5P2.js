63a213a4b4000f26376882a97b4a0e63e3c179eb
karfa113 <karfamonojit3117@gmail.com>
2025-08-22 12:28:51 +0530

    Day 05

---
+++ Day 05/D5P2.js
@@ -0,0 +1,13 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+for(let i =1; i<11; i++){
+    for(let j=1; j<11; j++){
+        console.log(`${i} * ${j} = ${i*j}`);     
+    }
+    console.log("----------");
+}
+
