cee43e7f8dc4bfd2da3a0c100473dff1574a58cf
karfa113 <karfamonojit3117@gmail.com>
2025-08-28 20:05:12 +0530

    Number Guessing Game

---
+++ Projects/04_NumberGuess/script.js
@@ -0,0 +1,15 @@
+const randomNum = parseInt(Math.random() * 100 + 1)
+
+const submit = document.querySelector('#submit')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('.guesses')
+const remaining = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let previousGuess = []
+let attempts = 1
+let playGame = true
+
